832ddfd98d60872e1da6ce6d534f1050af786e18
Henri Sara <hesara@vaadin.com>
2016-03-17 22:06:12 +0200

    Refactor Maven actions to check if goals enabled

--- com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/maven/MavenUtil.java
+++ com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/maven/MavenUtil.java
@@ -1,16 +1,29 @@
 package com.vaadin.integration.eclipse.maven;
 
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginExecution;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.m2e.actions.ExecutePomAction;
+import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.internal.IMavenConstants;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.IMavenProjectRegistry;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 
 public class MavenUtil {
+
+    private static final String VAADIN_MAVEN_PLUGIN_GROUP_ID = "com.vaadin";
+    private static final String VAADIN_MAVEN_PLUGIN_ARTIFACT_ID = "vaadin-maven-plugin";
+    private static final String VAADIN_MAVEN_PLUGIN_PREFIX = "vaadin:";
 
     public static boolean isMavenProject(IProject project) {
         if (project == null) {
@@ -48,22 +61,111 @@ public class MavenUtil {
         }
     }
 
+    /**
+     * Check whether a Maven project has a specific plug-in execution
+     * configured.
+     * 
+     * @param project
+     *            Maven project, not null
+     * @param pluginGroupId
+     *            Maven plug-in groupId to look for
+     * @param pluginArtifactId
+     *            Maven plug-in artifactIdd to look for
+     * @param goal
+     *            the Maven goal that whose execution is being looked for
+     * @return true if the project has the goal configured in the executions
+     *         list of the given plug-in
+     */
+    private static boolean definesPluginExecution(MavenProject project,
+            String pluginGroupId, String pluginArtifactId, String goal) {
+        if (project.getOriginalModel().getBuild() == null) {
+            return false;
+        }
+        for (Plugin p : project.getOriginalModel().getBuild().getPlugins()) {
+            if (p.getGroupId().equals(pluginGroupId)
+                    && p.getArtifactId().equals(pluginArtifactId)) {
+                for (PluginExecution execution : p.getExecutions()) {
+                    if (execution.getGoals().contains(goal)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static IMavenProjectFacade getMavenProjectFacade(IProject project) {
+        IMavenProjectRegistry projectManager = MavenPlugin
+                .getMavenProjectRegistry();
+        IFile pomFile = project
+                .getFile(new Path(IMavenConstants.POM_FILE_NAME));
+        IMavenProjectFacade projectFacade = projectManager.create(pomFile,
+                false, new NullProgressMonitor());
+        return projectFacade;
+    }
+
+    private static void runMavenGoals(IProject project, String pluginGroupId,
+            String pluginArtifactId, String prefix, String... goals)
+            throws CoreException {
+        IMavenProjectFacade facade = getMavenProjectFacade(project);
+        MavenProject mavenProject = facade
+                .getMavenProject(new NullProgressMonitor());
+        String enabledGoals = "";
+        String allGoals = "";
+        for (String goal : goals)
+         {
+            allGoals = allGoals + prefix + goal + " ";
+            if (definesPluginExecution(mavenProject, pluginGroupId,
+                    pluginArtifactId, goal)) {
+                enabledGoals = enabledGoals + prefix + goal
+                        + " ";
+            }
+        }
+        if (!enabledGoals.isEmpty()) {
+            runMavenGoal(project, enabledGoals);
+        } else {
+            // TODO iterate over subprojects and do the same
+
+            // final fallback: just execute all goals on the originally selected
+            // project
+            runMavenGoal(project, allGoals);
+        }
+
+    }
+
     public static boolean compileWidgetSet(IProject project) {
-        runMavenGoal(project, "vaadin:update-widgetset vaadin:compile");
-        return true;
+        try {
+            runMavenGoals(project, VAADIN_MAVEN_PLUGIN_GROUP_ID,
+                    VAADIN_MAVEN_PLUGIN_ARTIFACT_ID,
+                    VAADIN_MAVEN_PLUGIN_PREFIX, "update-widgetset",
+                    "compile");
+            return true;
+        } catch (CoreException e) {
+            return false;
+        }
     }
 
     public static boolean compileTheme(IProject project) {
-        runMavenGoal(project,
-                "vaadin:update-theme vaadin:compile-theme");
-        return true;
+        try {
+            runMavenGoals(project, VAADIN_MAVEN_PLUGIN_GROUP_ID,
+                    VAADIN_MAVEN_PLUGIN_ARTIFACT_ID,
+                    VAADIN_MAVEN_PLUGIN_PREFIX, "update-theme",
+                    "compile-theme");
+            return true;
+        } catch (CoreException e) {
+            return false;
+        }
     }
 
     public static boolean compileThemeAndWidgetset(IProject project) {
-        // TODO see above
-        runMavenGoal(
-                project,
-                "vaadin:update-theme vaadin:update-widgetset vaadin:compile-theme vaadin:compile");
-        return true;
+        try {
+            runMavenGoals(project, VAADIN_MAVEN_PLUGIN_GROUP_ID,
+                    VAADIN_MAVEN_PLUGIN_ARTIFACT_ID,
+                    VAADIN_MAVEN_PLUGIN_PREFIX, "update-theme",
+                    "update-widgetset", "compile-theme", "compile");
+            return true;
+        } catch (CoreException e) {
+            return false;
+        }
     }
 }
