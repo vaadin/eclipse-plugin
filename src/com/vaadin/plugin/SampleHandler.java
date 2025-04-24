0d80c41e962e1dd04840dc68b81a9cc37d58f0e4
marcin <marcin@vaadin.com>
2025-04-24 10:50:58 +0200

    Hello world plugin template

---
+++ src/com/vaadin/plugin/SampleHandler.java
@@ -0,0 +1,21 @@
+package com.vaadin.plugin;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class SampleHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Vaadin Eclipse Plugin",
+				"Hello, Eclipse world");
+		return null;
+	}
+}
