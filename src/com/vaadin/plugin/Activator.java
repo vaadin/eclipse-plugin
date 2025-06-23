66ce9aa9f5edfbff53e31af42a382e676fe2d352
Artur <artur@vaadin.com>
2025-06-23 13:38:55 +0300

    Use BundleActivator for servlet startup

--- src/com/vaadin/plugin/Activator.java
+++ src/com/vaadin/plugin/Activator.java
@@ -1,6 +1,6 @@
 package com.vaadin.plugin;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.http.NamespaceException;
 import javax.servlet.ServletException;
@@ -8,24 +8,11 @@ import javax.servlet.ServletException;
 /**
  * Bundle activator that starts the Copilot REST service on plugin startup.
  */
-public class Activator extends AbstractUIPlugin {
-    /** The plug-in ID as defined in MANIFEST.MF. */
-    public static final String PLUGIN_ID = "vaadin-eclipse-plugin";
-
-    private static Activator plugin;
+public class Activator implements BundleActivator {
     private CopilotRestService restService;
-
-    public Activator() {
-        plugin = this;
-    }
-
-    public static Activator getDefault() {
-        return plugin;
-    }
 
     @Override
     public void start(BundleContext context) throws Exception {
-        super.start(context);
         restService = new CopilotRestService();
         try {
             restService.start(context);
@@ -42,7 +29,5 @@ public class Activator extends AbstractUIPlugin {
             restService.stop(context);
             restService = null;
         }
-        plugin = null;
-        super.stop(context);
     }
 }
