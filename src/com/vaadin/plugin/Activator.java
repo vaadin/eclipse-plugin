e4415832777cc62c2d66821559d2729fce298dfb
Artur <artur@vaadin.com>
2025-06-23 18:13:30 +0300

    Start REST server using HttpServer

---
+++ src/com/vaadin/plugin/Activator.java
@@ -0,0 +1,27 @@
+package com.vaadin.plugin;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Bundle activator that starts the REST service when the plug-in is
+ * activated and stops it on shutdown.
+ */
+public class Activator implements BundleActivator {
+    private CopilotRestService restService;
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        restService = new CopilotRestService();
+        restService.start();
+        System.setProperty("vaadin.copilot.endpoint", restService.getEndpoint());
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        if (restService != null) {
+            restService.stop();
+            restService = null;
+        }
+    }
+}
---
+++ src/com/vaadin/plugin/EarlyStartup.java
@@ -0,0 +1,16 @@
+package com.vaadin.plugin;
+
+import org.eclipse.ui.IStartup;
+
+
+/**
+ * Ensures the plug-in is activated when the workbench starts so the
+ * {@link Activator} can launch the embedded REST service.
+ */
+public class EarlyStartup implements IStartup {
+
+    @Override
+    public void earlyStartup() {
+        // Trigger plug-in activation so the BundleActivator runs
+    }
+}
