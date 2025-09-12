20788d1edcaa475b9e22e831805538522af8593b
Manuel Carrasco Moñino <manolo@vaadin.com>
2025-09-12 14:49:37 +0200

    make assertion more readable

--- vaadin-eclipse-plugin.tests/src/com/vaadin/plugin/test/StarterProjectModelTest.java
+++ vaadin-eclipse-plugin.tests/src/com/vaadin/plugin/test/StarterProjectModelTest.java
@@ -67,7 +67,7 @@ public class StarterProjectModelTest {
 		model.setIncludeFlow(false);
 		model.setIncludeHilla(false);
 		String url = model.getDownloadUrl();
-		assertTrue("Shouldn't contain frameworks parameter", !url.matches(".*frameworks=.*"));
+		assertFalse("Shouldn't contain frameworks parameter", url.matches(".*frameworks=.*"));
 
 		// Test Flow only
 		model.setIncludeFlow(true);
